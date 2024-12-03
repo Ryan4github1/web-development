ec1d27c09b4bd411c3f0b6fefcce1b0b87885ae1
Ryan4github1 <ryan.singapur04@gmail.com>
2024-12-03 20:48:54 +1100

    palindrome using JS

---
+++ Array sorting and mapping in JavaScript after class/script.js
@@ -0,0 +1,6 @@
+var student = ['justin', 'bob', 'john', 'gerold']
+document.write(student + "<br><br>")
+document.write("student's name in ascending order:<br>")
+document.write(student.sort() + "<br><br>")
+document.write("student's name in descending order:<br>")
+document.write(student.reverse() + "<br><br>")
